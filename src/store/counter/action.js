c9603075fcd330bcf6bcc6af0175dd89fc3ac5a8
marufur-rahman <mdmarufur5682@gmail.com>
2023-06-14 16:57:10 +0600

    redux setup

---
+++ src/store/counter/action.js
@@ -0,0 +1,12 @@
+import { INCREMENT, DECREMENT } from "./actionType";
+
+export const increment = () => {
+	return {
+		type: INCREMENT,
+	};
+};
+export const decrement = () => {
+	return {
+		type: DECREMENT,
+	};
+};
